bd03b088594c2778836c01f74657b883753ce39f
1bro23 <shinlia21@gmail.com>
2021-09-11 16:42:30 +0800

    add tugas 6 php

---
+++ tugas6_1.php
@@ -0,0 +1,11 @@
+<?php
+$habib = 21;
+$Aji = $habib+4;
+$Bintan = $Aji+3;
+echo "====================================</br>";
+echo "Usia Habib = $habib</br>";
+echo "Usia Aji = $Aji</br>";
+echo "Usia Bintan = $Bintan</br>";
+echo "====================================</br>";
+
+ ?>
---
+++ tugas6_2.php
@@ -0,0 +1,10 @@
+<?php
+$jambu = 15000;
+$kardus = 2000;
+$total = $jambu*5*6+$kardus*2;
+echo "====================================</br>";
+echo "Harga Jambu = Rp $jambu / Kg</br>
+Harga Kardus = Rp $kardus /pcs</br>
+Total Penjualan (Dus dan Jambu) = Rp $total</br>";
+echo "====================================</br>";
+ ?>
